ca4eb3bff6935402382913f25fb85e586c350226
Samcoding5854 <samcoding5854@gmail.com>
2024-06-02 20:27:54 +0530

    Recent changes

--- transferDAta.py
+++ transferDAta.py
@@ -8,12 +8,12 @@ df = pd.read_csv(input_file)
 # Ensure the file has at least 30,000 rows
 if len(df) < 30000:
     raise ValueError("The input file must contain at least 30,000 rows")
-
+n = 200
 # Randomly sample 50 rows
-sampled_df = df.sample(n=10000, random_state=1)  # random_state is used for reproducibility
+sampled_df = df.sample(n=n, random_state=1)  # random_state is used for reproducibility
 
 # Save the sampled rows to a new CSV file
 output_file = 'output_file.csv'  # Replace with your desired output file name
 sampled_df.to_csv(output_file, index=False)
 
-print(f"50 random rows have been saved to {output_file}")
+print(f"{n} random rows have been saved to {output_file}")
